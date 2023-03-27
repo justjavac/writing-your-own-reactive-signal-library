60af9d2712ff246d4750b69eed18d7dcd725c12b
迷渡 <justjavac@gmail.com>
2023-03-27 13:44:09 +0800

    添加详细代码注释

--- signal.mjs
+++ signal.mjs
@@ -1,3 +1,7 @@
+/**
+ * 存储当前正在监听响应式值变化的函数
+ * @type {(() => void) | undefined}
+ */
 let currentListener = undefined;
 
 /**
@@ -8,40 +12,40 @@ let currentListener = undefined;
  */
 
 /**
- * `createSignal` will be used to read and set a reactive value.
+ * `createSignal` 用于读取和设置响应式的值。
  *
- * Example usage
+ * 示例用法：
  *
  * ```ts
  * const [count, setCount] = createSignal(0);
  * ```
  *
- * @param {T} initialValue
- * @returns {Signal<T>}
+ * @param {T} initialValue - 初始化的值
+ * @returns {Signal<T>} - 返回读写响应式值的函数组成的数组
  */
 export function createSignal(initialValue) {
   let value = initialValue;
 
   /**
-   * a set of callback functions, from createEffect
+   * 存储 createEffect 中传入的函数，这些函数会在值变化时被调用
    * @type {Set<() => void>}
    */
   const subscribers = new Set();
 
+  // 定义读取函数，如果当前正在监听，则将监听器函数存入 subscribers 中
   const read = () => {
     if (currentListener !== undefined) {
-      // before returning, track the current listener
       subscribers.add(currentListener);
     }
     return value;
   };
 
   /**
-   * @param {T} newValue
+   * @param {T} newValue - 要设置的新值
    */
   const write = (newValue) => {
     value = newValue;
-    // after setting the value, run any subscriber, aka effect, functions
+    // 值变化后，遍历 subscribers 调用每个监听器函数
     subscribers.forEach((fn) => fn());
   };
 
@@ -49,11 +53,11 @@ export function createSignal(initialValue) {
 }
 
 /**
- * `createEffect` will be used to run side effects whenever that value changes.
+ * `createEffect` 用于在响应式值发生变化时执行副作用。
  *
- * It will run the callback function whenever the value changes.
+ * 每当响应式值变化时，它都会运行传入的回调函数。
  *
- * Example usage
+ * 示例用法：
  *
  * ```ts
  * createEffect(() => {
@@ -61,10 +65,10 @@ export function createSignal(initialValue) {
  * });
  * ```
  *
- * @param {() => void} callback
+ * @param {() => void} callback - 要执行的回调函数
  */
 export function createEffect(callback) {
-  currentListener = callback;
-  callback();
-  currentListener = undefined;
+  currentListener = callback; // 记录当前正在运行的监听器函数
+  callback(); // 在当前监听器函数下执行回调函数
+  currentListener = undefined; // 在不需要监听器函数时避免将其添加到订阅者集合中
 }
